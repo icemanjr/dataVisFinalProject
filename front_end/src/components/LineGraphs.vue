dcab816ceb2540fde9ca3459d27f71237323a4c9
jaxon willard <jaxonbwillard@gmail.com>
2021-12-07 12:29:47 -0700

    remove commented code

--- front_end/src/components/LineGraphs.vue
+++ front_end/src/components/LineGraphs.vue
@@ -25,62 +25,13 @@ export default class LineGraphs extends Vue {
       return dat.RegionName === selectedState;
     }
     const filtered_data = data[this.category].filter(d => stateFilter(d))[0]
-    const filtered_data_keys = Object.keys(filtered_data).slice(7, filtered_data.length)
-
-
-    // const buyAvg = data["all_homes"].filter(stateFilter);
-    // const buy1 = data["one_bed"].filter(stateFilter);
-    // const buy2 = data["two_bed"].filter(stateFilter);
-    // const buy3 = data["three_bed"].filter(stateFilter);
-    // const buy4 = data["four_bed"].filter(stateFilter);
-    // const buy5 = data["five_plus_bed"].filter(stateFilter);
-    // const buy = [buyAvg[0], buy1[0], buy2[0], buy3[0], buy4[0], buy5[0]];
-
-    // const rentAvg = data["all_homes_rental"].filter(stateFilter);
-    // const rent1 = data["one_bed_rental"].filter(stateFilter);
-    // const rent2 = data["two_bed_rental"].filter(stateFilter);
-    // const rent3 = data["three_bed_rental"].filter(stateFilter);
-    // const rent4 = data["four_bed_rental"].filter(stateFilter);
-    // const rent5 = data["five_plus_bed_rental"].filter(stateFilter);
-    // const rent = [rentAvg[0], rent1[0], rent2[0], rent3[0], rent4[0], rent5[0]];
-
+    const filtered_data_keys = Object.keys(filtered_data).slice(7, filtered_data.length) // 7 is where the data starts... 
     const width = d3.select (`#line-graph${this.index}`).node().getBoundingClientRect().width;
     const height = d3.select(`#line-graph${this.index}`).node().getBoundingClientRect().height;
     const graphWidth = width/1.2 - this.margins.left - this.margins.right;
     const graphHeight = height/1.2 - this.margins.top - this.margins.bottom;
-    
-    // const titlesBuy = ["Buy Average", "Buy 1 Bedroom", "Buy 2 Bedroom", "Buy 3 Bedroom", "Buy 4 Bedroom", "Buy 5+ Bedroom"]
-    // const titlesRent = ["Rent Average", "Rent 1 Bedroom", "Rent 2 Bedroom", "Rent 3 Bedroom", "Rent 4 Bedroom", "Rent 5+ Bedroom"]
-
     const startIndex = 188;
     const endIndex = 295;
-
-    // const validKeysBuy = Object.keys(buyAvg[0]).slice(startIndex, endIndex);
-    // const validKeysRent = Object.keys(rentAvg[0]).slice(startIndex, endIndex);
-
-    // let maxBuy = 0;
-    // let maxRent = 0;
-    // let tempMax;
-    // for (let i=0; i<6; i++) {
-    //   tempMax = Object.values(buy[i]).slice(startIndex, endIndex).map(Number).reduce(function(a,b){return Math.max(a,b)});
-    //   if (maxBuy < tempMax) {
-    //     maxBuy = tempMax;
-    //   }
-    // }
-    // for (let i=0; i<6; i++) {
-    //   tempMax = Object.values(rent[i]).slice(startIndex, endIndex).map(Number).reduce(function(a,b){return Math.max(a,b)})
-    //   if (maxRent < tempMax) {
-    //     maxRent = tempMax;
-    //   }
-    // }
-
-    // function  getYValueBuy(key, idx) {
-    //   return +buy[idx][key];
-    // }
-
-    // function getYValueRent(key, idx) {
-    //   return +rent[idx][key];
-    // }
 
     const getYValue = (key) => filtered_data[key];
 
@@ -88,9 +39,6 @@ export default class LineGraphs extends Vue {
       const splitDate = key.split("-")
       return new Date(splitDate[0], splitDate[1] - 1, splitDate[2])
     }
-
-    //...
-
     const xScale = d3.scaleTime()
         .range([0, graphWidth])
         .domain(d3.extent(Object.keys(filtered_data), key => {
@@ -100,9 +48,6 @@ export default class LineGraphs extends Vue {
     const yScale = d3.scaleLinear()
         .range([graphHeight, 0])
         .domain([0, this.maxValue()]);
-
-
-
     const lineFunc = d3.line()
         .x(key => {
           return xScale(formatDate(key))
@@ -110,8 +55,6 @@ export default class LineGraphs extends Vue {
         .y((key) => {
           return yScale(getYValue(key))
         });
-
-
     const svg = d3.select(`#line-graph${this.index}`)
       .append("svg")
       .attr("width", graphWidth + this.margins.left + this.margins.right)
@@ -141,121 +84,6 @@ export default class LineGraphs extends Vue {
       .attr('stroke', 'black')
       .attr("stroke-width", 2)
       .attr("fill", "none");
-
-//...
-    // const buy0LineFunc = d3.line()
-    //   .x(key => {
-    //     console.log(xAxisBuy(formatDate(key)))
-    //     console.log(key, formatDate(key))
-    //     const r =  xAxisBuy(formatDate(key))
-    //     console.log(r)
-    //     return r
-    //   })
-    //   .y(key => {
-    //     console.log(key)
-    //     const r =  yAxisBuy(getYValueBuy0(key))
-    //     return r
-    //   })
-
-    
-
-
-    // for (let i=0; i<6; i++) {
-    //   let svgBuy = d3.select(`#line-graph-buy${i}`)
-    //       .append("svg")
-    //       .attr("width", graphWidth + this.margins.left + this.margins.right)
-    //       .attr("height", graphHeight + this.margins.top + this.margins.bottom);
-
-    //   let gBuy = svgBuy.append("g")
-    //       .attr("transform", "translate(" + this.margins.left + "," + this.margins.top + ")");
-
-    //   let yaxisBuy = d3.axisLeft().scale(yScaleBuy).ticks(5);
-    //   let xaxisBuy = d3.axisBottom().scale(xScaleBuy);
-
-    //   gBuy.append("g")
-    //     .attr("class", "axis")
-    //     .attr("transform", "translate(0," + graphHeight + ")")
-    //     .call(xaxisBuy)
-    //     .selectAll("text")
-    //     .attr("y", 0)
-    //     .attr("x", 9)
-    //     .attr("dy", ".35em")
-    //     .attr("transform", "rotate(90)")
-    //     .style("text-anchor", "start");
-
-    //   gBuy.append("g")
-    //     .attr("class", "axis")
-    //     .call(yaxisBuy);
-
-    //   console.log(buy[i])
-
-    //   gBuy.append('svg:path')
-    //     .data(buy[i])
-    //     // .attr("d", lineFuncBuy({key: validKeysBuy, idx: i}))
-    //     .attr("d", d => {
-    //       console.log(d)
-    //       return 10
-    //     })
-    //     .attr('stroke', 'black')
-    //     .attr('stroke-width', 2)
-    //     .attr('fill', 'none')
-    //     .attr("id", "line")
-    //     .style("z-index", "0")
-    //     .on("mouseover", function (d, e) {
-    //       console.log(this, d, e)
-    //     });
-
-    //   gBuy.append("text")
-    //     .attr("text-anchor", "middle")
-    //     .attr("x", graphWidth/2)
-    //     .attr("y", 0)
-    //     .style("font-family", "calibri")
-    //     .text(`${selectedState} ${titlesBuy[i]}`);
-
-    //   let svgRent = d3.selectAll(`#line-graph-rent${i}`)
-    //       .append("svg")
-    //       .attr("width", graphWidth + this.margins.left + this.margins.right)
-    //       .attr("height", graphHeight + this.margins.top + this.margins.bottom);
-
-    //   let gRent = svgRent.append("g")
-    //     .attr("transform", "translate(" + this.margins.left + "," + this.margins.top + ")");
-
-    //   let yaxisRent = d3.axisLeft().scale(yScaleRent).ticks(5);
-    //   let xaxisRent = d3.axisBottom().scale(xScaleRent);
-
-    //   gRent.append("g")
-    //     .attr("class", "axis")
-    //     .attr("transform", "translate(0," + graphHeight + ")")
-    //     .call(xaxisRent)
-    //     .selectAll("text")
-    //     .attr("y", 0)
-    //     .attr("x", 9)
-    //     .attr("dy", ".35em")
-    //     .attr("transform", "rotate(90)")
-    //     .style("text-anchor", "start");
-
-    //   gRent.append("g")
-    //     .attr("class", "axis")
-    //     .call(yaxisRent);
-
-    //   gRent.append('svg:path')
-    //     .attr("d", lineFuncRent({key: validKeysRent, idx: i}))
-    //     .attr('stroke', 'black')
-    //     .attr('stroke-width', 2)
-    //     .attr('fill', 'none')
-    //     .attr("id", "line")
-    //     .style("z-index", "0")
-    //     .on("mouseover", function (d, e) {
-    //       console.log(this, d, e)
-    //     });
-
-    //   gRent.append("text")
-    //     .attr("text-anchor", "middle")
-    //     .attr("x", graphWidth/2)
-    //     .attr("y", 0)
-    //     .style("font-family", "calibri")
-    //     .text(`${selectedState} ${titlesRent[i]}`);
-    // }
   }
   
   mounted() {
