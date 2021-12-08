729ee156bcd553cc1ec443d4bd77f37b0709f868
Parker-Krieger <a02317687@aggies.usu.edu>
2021-12-07 17:10:25 -0700

    Added header to visualization.

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app" style="height: 100vh;">
+    <h1 style="text-align: center">Housing Price Changes January 2010 - December 2019</h1>
     <router-view />
   </div>
 </template>
